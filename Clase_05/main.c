caee972af004fefe8b2a53622d5eefa3f113a31c
Guiillermo-Correa <36193837+Guiillermo-Correa@users.noreply.github.com>
2018-04-12 12:30:33 -0300

    Add files via upload

---
+++ Clase_05/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define cantidad 5
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ void cambiarEntero(int numero);
+
+ void cambiarVector(int listadoDeNumeros[]);
+
+
+
+
+
+int main()
+{
+    int edad=22;
+    int edades[3];
+
+    cambiarEntero(edad);
+    printf("edad: %d ",edad);
+
+    cambiarVector(edades);
+    printf("Edades: %d",edades);
+
+
+
+    return 0;
+}
+
+void cambiarEntero(int numero)
+{
+    numero=666;
+
+}
+
+void cambiarVector(int listadoDeNumeros[])
+{
+    listadoDeNumeros[0]=666;
+
+
+}*/
